c207a629c31dae09d37e7ab61db75fcb2083aa7d
Rahulalvk <rahulal432@gmail.com>
2023-04-28 01:01:01 -0700

    Programe know one line is equal, greater or less than the other line

--- LineComparisonComputation/LineComparisonComputation/EqualityOfLines.cs
+++ LineComparisonComputation/LineComparisonComputation/EqualityOfLines.cs
@@ -32,7 +32,7 @@ namespace LineComparisonComputation
             Console.WriteLine("x2= :" + x2b);
             int y2b = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("y2= :" + y2b);
-            float length2 = (float)Math.Sqrt((x2a - x1a) ^ 2 + (y2a - y1a) ^ 2);
+            float length2 = (float)Math.Sqrt((x2b - x1b) ^ 2 + (y2b - y1b) ^ 2);
             Console.WriteLine("The length of second line =" + length2);
 
             if (length1 == length2)
@@ -43,6 +43,15 @@ namespace LineComparisonComputation
             else
             {
                 Console.WriteLine("Two lines are not equal");
+            }
+            if (length1 > length2) 
+            {
+                Console.WriteLine("First line length  is greater than the second line");
+
+            }
+            else 
+            {
+                Console.WriteLine("Second line length is greater then first line");
             }
         }
     }
